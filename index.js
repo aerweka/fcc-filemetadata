b1a6bc3ca189a5d3d8fb4320be46a6f97cbe04ff
Alerio Reyhan <alerioreyhan127@gmail.com>
2023-11-26 08:45:21 +0700

    complete the task

--- index.js
+++ index.js
@@ -1,20 +1,32 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+var express = require("express");
+var cors = require("cors");
+const multer = require("multer");
+const upload = multer();
+const bodyParser = require("body-parser");
+require("dotenv").config();
 
 var app = express();
 
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
+const multerCfg = upload.single("upfile");
+app.post("/api/fileanalyse", multerCfg, function (req, res) {
+  const { originalname, mimetype, size } = req.file;
 
-
+  return res.json({
+    name: originalname,
+    type: mimetype,
+    size: size,
+  });
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  console.log("Your app is listening on port " + port);
 });
